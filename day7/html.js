f3fda8f8a0e029a30f9e98901a09e5083e92d271
githubscientist <sathish.datascientist@gmail.com>
2023-07-15 19:00:02 +0530

    html day 1

---
+++ day7/html.js
@@ -0,0 +1,11 @@
+/*
+    HTML - Hyper Text Markup Language
+
+    Markup languages tells web browser how to structure the web pages.
+
+    Examples:
+
+        1. HTML - Hyper Text Markup Language
+        2. XML - Extensible Markup Language
+        3. MD - Markdown Language
+*/
