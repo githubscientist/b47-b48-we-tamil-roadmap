d213b0bfd6692871d213ffd619a65280e6c7e39a
githubscientist <sathish.datascientist@gmail.com>
2023-08-05 18:38:49 +0530

    day 12

---
+++ day12/exercises.js
@@ -0,0 +1,13 @@
+/*
+    1. Create a simple responsive webpage using bootstrap. the webpage should have the following elements.
+
+        a. a heading with the text "Welcome to the Bootstrap Exercise" centered to the page
+        b. A paragraph with the text "This is a small exercise using Bootstrap" centered on the page
+        c. Two cards displayed side-by-side, each containing:
+            i. an image at the top
+            ii. a card title
+            iii. some text content
+            iv. a "read more" button at the bottom
+
+    Make sure the page looks visually appealing and that the cards stack on top of each other on smaller screens.
+*/
---
+++ day12/script.js
@@ -0,0 +1,8 @@
+// console.log('javascript works!');
+
+// document.getElementById('header').textContent = 'Hello, Javascript!';
+
+// select the element
+const header = document.getElementById('header');
+
+header.textContent = 'Hello, JavaScript!';
