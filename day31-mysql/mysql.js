9d324b0a942b19a7f19c6702016e3e7136f19b90
githubscientist <sathish.datascientist@gmail.com>
2023-11-04 19:05:43 +0530

    mysql day 2

--- day31-mysql/mysql.js
+++ day31-mysql/mysql.js
@@ -139,6 +139,171 @@
     to select age and email columns from students table:
     select age, email from students;
 
-    
+    MySQL Engines:
+        - Innodb
+        - MariaDB
+        - etc.
 
+    Exercise: 
+        From the mysqlsampledatabase, with the payments table,
+            1. select all the records
+
+            query: select * from payments;
+
+            2. select all the records where amount is greater than 100000
+
+            query: select * from payments where amount > 100000;
+
+            3. select all the records where amount is greater than 100000 and less than 200000
+
+            query: select * from payments where amount > 100000 and amount < 200000;
+
+            4. group by customerNumber and find the total amount for each customer
+
+            query: select customerNumber, sum(amount) from payments group by customerNumber;
+
+            5. group by customerNumber and find the total amount for each customer where the total amount is greater than 100000
+
+            query: select customerNumber, sum(amount) from payments group by customerNumber having sum(amount) > 100000;
+
+            6. arrange the records in descending order of amount
+
+            query: select customerNumber, sum(amount) from payments group by customerNumber having sum(amount) > 100000 order by sum(amount) desc;
+
+            7. select the top 3 and second top 3 records
+
+            - top3 table has the top 3 records
+            - customers table has the customer details
+
+            Now, we need to join both the tables to find the customer details of the top 3 customers (customer name and phone number).
+
+            query: select c.customerName, c.phone from customers c join top3 t on c.customerNumber = t.customerNumber;
+*/
+
+/*
+    Exercise: Design a database for an online bookstore.
+
+    - the bookstore sells books and each book can belong to multiple genres.
+    - customers can place orders and each order can contain multiple books.
+    - each customer can have multiple addresses.
+    - an address can be associated with multiple customers.
+    - each book has multiple authors.
+    - an author can write multiple books.
+
+    List of tables:
+        - books
+        - genres
+        - authors
+        - customers
+        - orders
+        - addresses
+
+    Relationships:
+        - books and genres: many-to-many
+        - books and authors: many-to-many
+        - customers and addresses: many-to-many
+        - customers and orders: one-to-many
+    
+    List of Tables and columns:
+        1. books
+            - bookID (primary key)
+            - genreID (foreign key referencing genres table)
+            - authorID (foreign key referencing authors table)
+            - title
+            - ISBN
+            - price
+            - publicationDate
+        
+        2. genres
+            - genreID (primary key)
+            - name
+        
+        3. authors
+            - authorID (primary key)
+            - bookID (foreign key referencing books table)
+            - name
+            - noOfPublications
+        
+        4. customers
+            - customerID (primary key)
+            - addressID (foreign key referencing addresses table)
+            - customerName
+            - customerEmail
+            - customerPhone
+        
+        5. addresses
+            - addressID (primary key)
+            - customerID (foreign key)
+            - addressLine1
+            - addressLine2
+            - city
+            - state
+            - postalcode
+            - country
+        
+        6. orders
+            - orderID (primary key)
+            - customerID (foreign key referencing customers table)
+            - bookID (foreign key referencing books table)
+            - orderDate
+            - orderTime
+            - orderAmount
+            - orderQuantity
+*/
+
+/*
+        Normalization:
+            - Normalization is a process of organizing the data in the database.
+            - The goal of normalization is to reduce data redundancy (duplicate data) and improve data integrity (accuracy and consistency)
+            - Normalization is a multi-step process that puts data into tabular form by removing duplicated data from the relation tables.
+            
+            - There are 5 normal forms: 
+            1NF, 
+            2NF, 
+            3NF, 
+            4NF, 
+            5NF
+
+            - 1NF: Eliminate repeating groups in individual tables
+
+                - All columns must contain atomic values
+                - Each column should have a unique name
+                - the order in which data is stored does not matter
+
+                example: 
+                    
+                studentCourses
+                  - studentID
+                  - course1
+                  - course2
+                  - studentName
+                
+                table: students
+                    - studentID
+                    - studentName
+                
+                table: courses
+                    - courseID
+                    - courseName
+
+            - 2NF: Eliminate redundant data
+            - 3NF: Eliminate columns not dependent on key
+            - BCNF: Boyce-Codd Normal Form
+            - 4NF: Multivalued dependency
+            - 5NF: Join dependency
+*/
+
+/*
+        mysql summarize functions:
+        
+        1. sum
+        2. avg
+        3. min
+        4. max
+        5. count
+        6. stddev
+        7. variance
+        8. distinct
+        9. logavg
+        10. logsum
 */
